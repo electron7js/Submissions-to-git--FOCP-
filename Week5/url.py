fd3f3afba121d3ef7efc2a0baf9ec1dc40be3330
electron7js <bijeshmansth.bs@gmail.com>
2022-11-20 23:57:54 +0545

    updated url.py and renamed copy.py to copyfile.py because of collision

--- Week5/url.py
+++ Week5/url.py
@@ -3,6 +3,9 @@ import requests
 url=sys.argv[1]
 try :
     response=requests.get(url)
-    print("HTTP Response code:",response.status_code)
+    if(response.status_code==200):
+        print("There is a working website at this url")
+    else:
+        print("Website is down")
 except:
     print("error, wrong url, or no connection")
